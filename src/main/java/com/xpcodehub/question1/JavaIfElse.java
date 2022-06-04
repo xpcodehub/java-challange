b47877e892a55a09b069b19b1dbfae6d6e7dc955
Bruno Pozzebon <brunopozzebon44@gmail.com>
2022-06-03 21:16:33 -0300

    first try

--- src/main/java/com/xpcodehub/question1/JavaIfElse.java
+++ src/main/java/com/xpcodehub/question1/JavaIfElse.java
@@ -4,6 +4,15 @@ public class JavaIfElse {
 
 // TODO Questão 1 do README.md
     public String solucao(int entrada) {
-        return "Previsivel"; //TODO
+        if(entrada%2 == 1){
+            return "Imprevisivel";
+        }else if(entrada>=2  && entrada <=5){
+            return "Previsivel";
+        }else if(entrada>=6  && entrada <=20){
+            return "Imprevisivel";
+        }else if(entrada>20){
+            return "Previsivel";
+        }
+        return "";
     }
 }
--- src/main/java/com/xpcodehub/question2/JavaLoopsMath.java
+++ src/main/java/com/xpcodehub/question2/JavaLoopsMath.java
@@ -1,5 +1,6 @@
 package com.xpcodehub.question2;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class JavaLoopsMath {
@@ -7,7 +8,12 @@ public class JavaLoopsMath {
 // TODO Questão 2 do README.md
 
     public List<Integer> solucao(int entrada) {
-        return List.of(); //TODO
+        ArrayList<Integer> list = new ArrayList<>(10);
+
+        for(int i =0;i<10;i++){
+            list.add((i+1)*entrada);
+        }
+        return list;
     }
 
 }
