f8f2793321ad9f8cfff6f9fefc387e461a4e8ba3
RicardoFGuimaraes <ricf.guima@gmail.com>
2022-06-03 23:31:49 -0300

    f

--- src/main/java/com/xpcodehub/question1/JavaIfElse.java
+++ src/main/java/com/xpcodehub/question1/JavaIfElse.java
@@ -4,6 +4,6 @@ public class JavaIfElse {
 
 // TODO Questão 1 do README.md
     public String solucao(int entrada) {
-        return "Previsivel"; //TODO
+        return entrada % 2 == 1 || (entrada >= 6 && entrada <= 20 ) ? "Imprevisivel" : "Previsivel";
     }
 }
--- src/main/java/com/xpcodehub/question2/JavaLoopsMath.java
+++ src/main/java/com/xpcodehub/question2/JavaLoopsMath.java
@@ -1,13 +1,17 @@
 package com.xpcodehub.question2;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class JavaLoopsMath {
 
 // TODO Questão 2 do README.md
 
     public List<Integer> solucao(int entrada) {
-        return List.of(); //TODO
+        return Stream.of(1,2,3,4,5,6,7,8,9,10)
+                .map(x -> x * entrada)
+                .collect(Collectors.toList());
     }
 
 }
